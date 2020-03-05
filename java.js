15c672f9fd809fad2a7a1f65fb455f7e9da914a2
nadiamestrado <56513779+nadiamestrado@users.noreply.github.com>
2020-03-05 01:16:04 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function showImg() {
+  var x = document.getElementById("divimg");
+console.log('iiii');
+
+  if (x.style.display != "block") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
